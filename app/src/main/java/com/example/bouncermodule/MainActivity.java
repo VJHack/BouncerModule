2fa1ff201add9f08b2d42e6b6bc42a1b77cf615e
VJHack <flymyplane21@gmail.com>
2023-03-27 17:16:11 -0500

    Initial commit

---
+++ app/src/main/java/com/example/bouncermodule/MainActivity.java
@@ -0,0 +1,122 @@
+package com.example.bouncermodule;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.bouncermodule.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ActivityMainBinding binding;
+    private TextView counterValue;
+    private TextView currentLength;
+    private Button noneButton;
+    private Button shortButton;
+    private Button mediumButton;
+    private Button longButton;
+
+    private Button plusButton;
+    private Button minusButton;
+
+    private int counterValInt = 0;
+
+    private ImageView photoIdImageView;
+    private Button photoIdButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        noneButton = (Button) findViewById(R.id.None);
+        noneButton.setOnClickListener(this);
+        shortButton = (Button) findViewById(R.id.Short);
+        shortButton.setOnClickListener(this);
+        mediumButton = (Button) findViewById(R.id.Medium);
+        mediumButton.setOnClickListener(this);
+        longButton = (Button) findViewById(R.id.Long);
+        longButton.setOnClickListener(this);
+
+        currentLength = (TextView) findViewById(R.id.CurrentLengthValue);
+        counterValue = (TextView) findViewById(R.id.Total_Value);
+        plusButton = (Button) findViewById(R.id.plus);
+        plusButton.setOnClickListener(this);
+        minusButton = (Button) findViewById(R.id.minus);
+        minusButton.setOnClickListener(this);
+
+        photoIdImageView = findViewById(R.id.imageviewphotoid);
+        photoIdButton = findViewById(R.id.capturePhotoId);
+
+        //Request for camera runtime permission
+//        if (ContextCompat.checkSelfPermission( MainActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+//            ActivityCompat.requestPermissions(MainActivity.this, new String[]{
+//                    Manifest.permission.CAMERA
+//            },100);
+//        }
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_facerecognition)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(binding.navView, navController);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.Short) {
+            currentLength.setText("SHORT");
+            currentLength.setTextColor(Color.parseColor("#0000FF"));
+        }else if(view.getId() == R.id.Medium){
+            currentLength.setText("MEDIUM");
+            currentLength.setTextColor(Color.parseColor("#FFA500"));
+        }else if(view.getId() == R.id.Long){
+            currentLength.setText("LONG");
+            currentLength.setTextColor(Color.parseColor("#FF0000"));
+        }else if(view.getId() == R.id.None){
+            currentLength.setText("NONE");
+            currentLength.setTextColor(Color.parseColor("#028A0F"));
+        }else if(view.getId() == R.id.plus){
+            counterValInt++;
+            counterValue.setText("Total:    " +String.valueOf(counterValInt));
+        }else if(view.getId() == R.id.minus){
+            if(counterValInt >= 1) {
+                counterValInt--;
+            }
+            counterValue.setText("Total:    " + String.valueOf(counterValInt));
+        }else if(view.getId() == R.id.imageviewphotoid){
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(intent, 100);
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 100) {
+            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+            photoIdImageView.setImageBitmap(bitmap);
+        }
+    }
+
+}
