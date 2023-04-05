92a6e7960e893e1bfaf214347c88cc8af6c2eb3c
VJHack <flymyplane21@gmail.com>
2023-04-05 16:11:33 -0500

    List is up

--- app/src/main/java/com/example/bouncermodule/ui/bars/BarsFragment.java
+++ app/src/main/java/com/example/bouncermodule/ui/bars/BarsFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,6 +15,8 @@ import androidx.fragment.app.FragmentActivity;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.bouncermodule.R;
+import com.example.bouncermodule.databinding.FragmentBarsBinding;
+import com.example.bouncermodule.databinding.FragmentHomeBinding;
 import com.example.bouncermodule.databinding.FragmentMapBinding;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -22,36 +26,71 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class BarsFragment extends Fragment implements OnMapReadyCallback {
+import java.util.ArrayList;
+import java.util.List;
 
-    private GoogleMap mMap;
-
-    private FragmentMapBinding binding;
+public class BarsFragment extends Fragment  {
 
 
+    private FragmentBarsBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         BarsViewModel barsViewModel =
                 new ViewModelProvider(this).get(BarsViewModel.class);
 
-        binding = FragmentMapBinding.inflate(inflater, container, false);
+        binding = FragmentBarsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        View contentView = inflater.inflate(R.layout.fragment_bars, container, false);
+        ListView listView = contentView.findViewById(R.id.bar_list);
+        // sample data
+        List<String> list = new ArrayList<>();
+        for(int i=0;i<100;i++)
+            list.add("Item "+i);
 
-//        final TextView textView = binding.textBars;
-//        barsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-//        barsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        CustomAdapter listAdapter = new CustomAdapter(list);
+        listView.setAdapter(listAdapter);
 
-        SupportMapFragment BarsFragment = (SupportMapFragment) getParentFragmentManager()
-                .findFragmentById(R.id.map);
-        System.out.println("Barrrrrrrrrrrrr  " + BarsFragment);
+
+        System.out.println("left tab  ");
 ////        BarsFragment.getMapAsync(this);
 
 
-        return root;
+        return contentView;
     }
 
+    class CustomAdapter extends BaseAdapter {
+        List<String> items;
 
+        public CustomAdapter(List<String> items) {
+            super();
+            this.items = items;
+        }
+
+        @Override
+        public int getCount() {
+            return items.size();
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return items.get(i);
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return items.get(i).hashCode();
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            TextView textView = new TextView(getContext());
+            textView.setText(items.get(i));
+            textView.setText("Bar Name:         Kollege Klub\nWait Time:         35 Minutes");
+
+            return textView;
+        }
+    }
 
     @Override
     public void onDestroyView() {
@@ -59,41 +98,7 @@ public class BarsFragment extends Fragment implements OnMapReadyCallback {
         binding = null;
     }
 
-//    @Override
-//    public boolean onMarkerClick(@NonNull Marker marker) {
-//        return false;
-//    }
 
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(43.0722, -89.4008);
-        mMap.addMarker(new MarkerOptions()
-                .position(sydney)
-                .title("Current location"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 15));
 
-//
-//        for(int i = 0; i < bars.length; i++){
-//            LatLng barLoc = new LatLng( (double) bars[i][0] ,  (double) bars[i][1]);
-//            testMarker = mMap.addMarker(new MarkerOptions()
-//                    .position(barLoc)
-//                    .snippet("(608)-098-2647")
-//
-//                    .title(bars[i][2].toString()));
-//            testMarker.setTag(0);
-//        }
-//        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
-//            @Override
-//            public boolean onMarkerClick(Marker marker) {
-//                // on marker click we are getting the title of our marker
-//                // which is clicked and displaying it in a toast message.
-//                String markerName = marker.getTitle();
-////                Toast.makeText(BarsFragment.this, "Clicked location is " + markerName, Toast.LENGTH_LONG).show();
-//                return false;
-//            }
-//        });
-    }
 }
--- app/src/main/java/com/example/bouncermodule/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/bouncermodule/ui/dashboard/DashboardFragment.java
@@ -12,11 +12,18 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.example.bouncermodule.R;
 import com.example.bouncermodule.databinding.FragmentMapBinding;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class DashboardFragment extends Fragment {
+public class DashboardFragment extends Fragment implements OnMapReadyCallback {
 
     private FragmentMapBinding binding;
+    private GoogleMap mMap;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -28,17 +35,55 @@ public class DashboardFragment extends Fragment {
 
 
 
-        SupportMapFragment BarsFragment = (SupportMapFragment) getParentFragmentManager()
+        SupportMapFragment dashboardFramgment = (SupportMapFragment) getParentFragmentManager()
                 .findFragmentById(R.id.map);
-        System.out.println("Barrrrrrrrrrrrr  " + BarsFragment);
+        System.out.println("Barrrrrrrrrrrrr  " + dashboardFramgment);
 //        final TextView textView = binding.textDashboard;
 //        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
+    }
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(43.0722, -89.4008);
+        mMap.addMarker(new MarkerOptions()
+                .position(sydney)
+                .title("Current location"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 15));
+
+//
+//        for(int i = 0; i < bars.length; i++){
+//            LatLng barLoc = new LatLng( (double) bars[i][0] ,  (double) bars[i][1]);
+//            testMarker = mMap.addMarker(new MarkerOptions()
+//                    .position(barLoc)
+//                    .snippet("(608)-098-2647")
+//
+//                    .title(bars[i][2].toString()));
+//            testMarker.setTag(0);
+//        }
+//        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+//            @Override
+//            public boolean onMarkerClick(Marker marker) {
+//                // on marker click we are getting the title of our marker
+//                // which is clicked and displaying it in a toast message.
+//                String markerName = marker.getTitle();
+////                Toast.makeText(BarsFragment.this, "Clicked location is " + markerName, Toast.LENGTH_LONG).show();
+//                return false;
+//            }
+//        });
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
+        if (mMap != null) {
+            getFragmentManager().beginTransaction()
+                    .remove(getFragmentManager().findFragmentById(R.id.map))
+                    .commit();
+
+        }
         binding = null;
     }
 }
