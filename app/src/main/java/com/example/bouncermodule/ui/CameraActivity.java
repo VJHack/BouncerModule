abca2d29cd4cfbf9464841abea3a769089a482f2
riley-griffin <rileygriffin00@gmail.com>
2023-04-24 17:41:11 -0500

    Removing some warnings and cleaning up code a bit. Have to merge this in next.

--- app/src/main/java/com/example/bouncermodule/ui/CameraActivity.java
+++ app/src/main/java/com/example/bouncermodule/ui/CameraActivity.java
@@ -5,34 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.camera.core.CameraSelector;
 import androidx.camera.core.ImageAnalysis;
 import androidx.camera.core.ImageCapture;
-import androidx.camera.core.ImageCaptureException;
 import androidx.camera.core.ImageProxy;
 import androidx.camera.core.Preview;
 import androidx.camera.lifecycle.ProcessCameraProvider;
 import androidx.camera.view.PreviewView;
 import androidx.core.content.ContextCompat;
 import androidx.lifecycle.LifecycleOwner;
-
 import android.content.pm.ApplicationInfo;
-import android.content.pm.ComponentInfo;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Rect;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.StrictMode;
-import android.util.Base64;
 import android.util.Log;
 import android.util.Size;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.OrientationEventListener;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.rekognition.AmazonRekognition;
 import com.amazonaws.services.rekognition.AmazonRekognitionClient;
@@ -41,18 +33,6 @@ import com.amazonaws.services.rekognition.model.CompareFacesRequest;
 import com.amazonaws.services.rekognition.model.CompareFacesResult;
 import com.amazonaws.services.rekognition.model.ComparedFace;
 import com.amazonaws.services.rekognition.model.Image;
-import com.android.volley.AuthFailureError;
-import com.android.volley.DefaultRetryPolicy;
-import com.android.volley.NetworkResponse;
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.VolleyLog;
-import com.android.volley.toolbox.HttpHeaderParser;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
 import com.example.bouncermodule.R;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -63,23 +43,14 @@ import com.google.mlkit.vision.face.Face;
 import com.google.mlkit.vision.face.FaceDetection;
 import com.google.mlkit.vision.face.FaceDetector;
 import com.google.mlkit.vision.face.FaceDetectorOptions;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import java.io.ByteArrayOutputStream;
-import java.io.FileInputStream;
 import java.nio.ByteBuffer;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Properties;
+import java.util.Objects;
 import java.util.concurrent.ExecutionException;
 
 public class CameraActivity extends AppCompatActivity implements ImageAnalysis.Analyzer {
     private PreviewView previewView;
-    private Preview preview;
-    private ImageCapture imageCapture;
     private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
     public static boolean firstImageCaptured = false;
     public static InputImage inputImage1;
@@ -92,61 +63,53 @@ public class CameraActivity extends AppCompatActivity implements ImageAnalysis.A
     private static byte[] bytes1;
     private static ByteArrayOutputStream out2;
     private static byte[] bytes2;
-    public Integer faceComparisonConfidence;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
         setContentView(R.layout.activity_camera);
         previewView = findViewById(R.id.previewView);
         cameraProviderFuture = ProcessCameraProvider.getInstance(this);
-        cameraProviderFuture.addListener(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
-                    bindImageAnalysis(cameraProvider);
-                } catch (ExecutionException | InterruptedException e) {
-                    e.printStackTrace();
-                }
+        cameraProviderFuture.addListener(() -> {
+            try {
+                ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+                bindImageAnalysis(cameraProvider);
+            } catch (ExecutionException | InterruptedException e) {
+                e.printStackTrace();
             }
         }, ContextCompat.getMainExecutor(this));
         Button photoButton = (Button) findViewById(R.id.takePhotoButton);
-        photoButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Button takePhotoButton = findViewById(R.id.takePhotoButton);
-                // if we haven't captured first image yet, set first image
-                if (!firstImageCaptured) {
-                    firstImageCaptured = true;
-                    //takePhoto(img1);
-                    img1 = previewView.getBitmap();
-                    // set image just to see if it worked
-                    inputImage1 = InputImage.fromBitmap(img1, 0);
-                    Log.i("IMAGES", "Image 1 captured");
-                    takePhotoButton.setText("Take ID Photo");
-                }
-                // if first image has been captured, set second image
-                else {
-                    firstImageCaptured = false;
-                    img2 = previewView.getBitmap();
-                    inputImage2 = InputImage.fromBitmap(img2, 0);
-                    Log.i("IMAGES", "Image 2 captured");
-                    takePhotoButton.setVisibility(View.INVISIBLE);
-                    previewView.setVisibility(View.INVISIBLE);
-                    checkFaces();
-                }
+        photoButton.setOnClickListener(v -> {
+            Button takePhotoButton = findViewById(R.id.takePhotoButton);
+            // if we haven't captured first image yet, set first image
+            if (!firstImageCaptured) {
+                firstImageCaptured = true;
+                //takePhoto(img1);
+                img1 = previewView.getBitmap();
+                // set image just to see if it worked
+                inputImage1 = InputImage.fromBitmap(img1, 0);
+                Log.i("IMAGES", "Image 1 captured");
+                takePhotoButton.setText("Take ID Photo");
+            }
+            // if first image has been captured, set second image
+            else {
+                firstImageCaptured = false;
+                img2 = previewView.getBitmap();
+                inputImage2 = InputImage.fromBitmap(img2, 0);
+                Log.i("IMAGES", "Image 2 captured");
+                takePhotoButton.setVisibility(View.INVISIBLE);
+                previewView.setVisibility(View.INVISIBLE);
+                checkFaces();
             }
         });
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                finish();
-                return true;
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
         }
 
         return super.onOptionsItemSelected(item);
@@ -161,18 +124,13 @@ public class CameraActivity extends AppCompatActivity implements ImageAnalysis.A
         ImageAnalysis imageAnalysis =
                 new ImageAnalysis.Builder().setTargetResolution(new Size(640, 480))
                         .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST).build();
-        imageAnalysis.setAnalyzer(ContextCompat.getMainExecutor(this), new ImageAnalysis.Analyzer() {
-            @Override
-            public void analyze(ImageProxy image) {
-                image.close();
-            }
-        });
-        preview = new Preview.Builder().build();
+        imageAnalysis.setAnalyzer(ContextCompat.getMainExecutor(this), ImageProxy::close);
+        Preview preview = new Preview.Builder().build();
         CameraSelector cameraSelector = new CameraSelector.Builder()
                 .requireLensFacing(CameraSelector.LENS_FACING_BACK).build();
         preview.setSurfaceProvider(previewView.getSurfaceProvider());
         ImageCapture.Builder builder = new ImageCapture.Builder();
-        imageCapture = builder.build();
+        ImageCapture imageCapture = builder.build();
         cameraProvider.bindToLifecycle((LifecycleOwner) this, cameraSelector,
                 imageAnalysis, preview, imageCapture);
     }
@@ -184,14 +142,6 @@ public class CameraActivity extends AppCompatActivity implements ImageAnalysis.A
         // TODO: this rotation degree might not be correct every time, look into more if we have time
         InputImage in1 = InputImage.fromBitmap(bitmap, image.getImageInfo().getRotationDegrees());
         Log.i("Riley Tag", "Made it here");
-        if (bitmap == null)
-            return;
-        /*runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                image1View.setImageBitmap(bitmap1);
-            }
-        });*/
     }
 
     public void checkFaces() {
@@ -204,59 +154,42 @@ public class CameraActivity extends AppCompatActivity implements ImageAnalysis.A
         Task<List<Face>> result1 =
                 detector.process(inputImage1)
                         .addOnSuccessListener(
-                                new OnSuccessListener<List<Face>>() {
-                                    @Override
-                                    public void onSuccess(List<Face> faces) {
-                                        Log.i("Face Detection", "Face Detection Succeeded, Found " + faces.size() + " faces");
-                                        for (Face face : faces) {
-                                            Rect bounds = face.getBoundingBox();
-                                            outputImage1 = Bitmap.createBitmap(img1, bounds.left, bounds.top, bounds.width(), bounds.height());
-                                            Log.i("Face Detection", "Bounding box: " + bounds.toString());
-                                            break;
-                                        }
-                                        ImageView iv1 = (ImageView) findViewById(R.id.image1View);
-                                        iv1.setImageBitmap(outputImage1);
-                                        out1 = new ByteArrayOutputStream();
-                                        outputImage1.compress(Bitmap.CompressFormat.PNG, 100, out1);
-                                        bytes1 = out1.toByteArray();
+                                faces -> {
+                                    Log.i("Face Detection", "Face Detection Succeeded, Found " + faces.size() + " faces");
+                                    for (Face face : faces) {
+                                        Rect bounds = face.getBoundingBox();
+                                        outputImage1 = Bitmap.createBitmap(img1, bounds.left, bounds.top, bounds.width(), bounds.height());
+                                        Log.i("Face Detection", "Bounding box: " + bounds);
+                                        break;
                                     }
+                                    ImageView iv1 = (ImageView) findViewById(R.id.image1View);
+                                    iv1.setImageBitmap(outputImage1);
+                                    out1 = new ByteArrayOutputStream();
+                                    outputImage1.compress(Bitmap.CompressFormat.PNG, 100, out1);
+                                    bytes1 = out1.toByteArray();
                                 })
                         .addOnFailureListener(
-                                new OnFailureListener() {
-                                    @Override
-                                    public void onFailure(@NonNull Exception e) {
-                                        Log.i("Face Detection", "Face Detection Failed");
-                                    }
-                                });
+                                e -> Log.i("Face Detection", "Face Detection Failed"));
         Task<List<Face>> result2 =
                 detector.process(inputImage2)
                         .addOnSuccessListener(
-                                new OnSuccessListener<List<Face>>() {
-                                    @Override
-                                    public void onSuccess(List<Face> faces) {
-                                        Log.i("Face Detection", "Face Detection Succeeded, Found " + faces.size() + " faces");
-                                        for (Face face : faces) {
-                                            Rect bounds = face.getBoundingBox();
-                                            outputImage2 = Bitmap.createBitmap(img2, bounds.left, bounds.top, bounds.width(), bounds.height());
-                                            Log.i("Face Detection", "Bounding box: " + bounds.toString());
-                                            break;
-                                        }
-                                        ImageView iv2 = (ImageView) findViewById(R.id.image2View);
-                                        iv2.setImageBitmap(outputImage2);
-                                        out2 = new ByteArrayOutputStream();
-                                        outputImage2.compress(Bitmap.CompressFormat.PNG, 100, out2);
-                                        bytes2 = out2.toByteArray();
-                                        // For facial comparison -----
-                                        compareFaces();
+                                faces -> {
+                                    Log.i("Face Detection", "Face Detection Succeeded, Found " + faces.size() + " faces");
+                                    for (Face face : faces) {
+                                        Rect bounds = face.getBoundingBox();
+                                        outputImage2 = Bitmap.createBitmap(img2, bounds.left, bounds.top, bounds.width(), bounds.height());
+                                        Log.i("Face Detection", "Bounding box: " + bounds);
+                                        break;
                                     }
+                                    ImageView iv2 = (ImageView) findViewById(R.id.image2View);
+                                    iv2.setImageBitmap(outputImage2);
+                                    out2 = new ByteArrayOutputStream();
+                                    outputImage2.compress(Bitmap.CompressFormat.PNG, 100, out2);
+                                    bytes2 = out2.toByteArray();
+                                    // For facial comparison -----
+                                    compareFaces();
                                 })
-                        .addOnFailureListener(
-                                new OnFailureListener() {
-                                    @Override
-                                    public void onFailure(@NonNull Exception e) {
-                                        Log.i("Face Detection", "Face Detection Failed");
-                                    }
-                                });
+                        .addOnFailureListener(e -> Log.i("Face Detection", "Face Detection Failed"));
     }
 
     public void compareFaces() {
